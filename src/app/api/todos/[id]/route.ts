ee9354e83bc3bb3c532d840c9818c2fd4ef66822
Victoria <ezeonuoguvictoria99@gmail.com>
2025-09-25 10:56:39 +0100

    fixing route deployment issues

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,12 +1,13 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function PUT(
-  req: Request,
-  { params }: { params: { id: string } }
+  req: NextRequest,
+  context: { params: Promise<{ id: string }> }
 ) {
+  const { id } = await context.params;
   const body = await req.json();
 
-  const res = await fetch(`https://dummyjson.com/todos/${params.id}`, {
+  const res = await fetch(`https://dummyjson.com/todos/${id}`, {
     method: "PUT",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(body),
